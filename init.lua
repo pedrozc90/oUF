642da129d1f79dfb49219e91209a3317279555a7
pedrozc90 <pedrozc90@gmail.com>
2023-09-06 23:00:14 -0300

    adding wow version variables

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 local _, ns = ...
 ns.oUF = {}
 ns.oUF.Private = {}
+
+local Interface = select(4, GetBuildInfo())
+
+ns.oUF.Interface = Interface
+ns.oUF.isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
+ns.oUF.isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
+ns.oUF.isBCC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
+ns.oUF.isWotLK = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
+ns.oUF.isDF = (Interface >= 100000 and Interface < 110000)
